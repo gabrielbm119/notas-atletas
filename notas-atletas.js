15424e57a0f28ff424ff2001abbb5fed0daab130
gabrielbm119 <110247164+gabrielbm119@users.noreply.github.com>
2022-12-30 03:39:45 -0300

    Create notas-atletas.js

---
+++ notas-atletas.js
@@ -0,0 +1,41 @@
+
+
+
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+
+/*
+A saída deve ser:
+
+Atleta: Cesar Abascal
+Notas Obtidas: 10,10,7.88,8.42,9.34
+Média Válida: 8.766666666666666
+
+Atleta: Fernando Puntel
+Notas Obtidas: 10,10,7,8,9.33
+Média Válida: 8.333333333333334
+
+Atleta: Daiane Jelinsky
+Notas Obtidas: 10,7,8,9.5,9.5
+Média Válida: 8.166666666666666
+
+Atleta: Bruno Castro
+Notas Obtidas: 10,10,10,9,9.5
+Média Válida: 9.666666666666666
+*/
