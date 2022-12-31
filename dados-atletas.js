c4ee33b541a0ab08d24f4d0153c4eaa1f6de92b2
gabrielbm119 <110247164+gabrielbm119@users.noreply.github.com>
2022-12-31 11:46:44 -0300

    Update dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,78 @@
+
+class Atleta {
+    constructor (nome, idade, peso, altura, notas){
+        this.nome = nome
+        this.idade = idade
+        this.peso = peso
+        this.altura = altura
+        this.notas = notas
+    }
+
+    calculaCategoria(){
+
+    }
+
+    calculaCategoria(){
+
+    }
+
+    calculaMediaValida(){
+
+    }
+
+    obtemNomeAtleta(){
+
+    }
+
+    obtemIdadeAtleta(){
+
+    }
+
+    obtemPesoAtleta(){
+
+    }
+
+    obtemNotasAtleta(){
+
+    }
+
+    obtemCategoria(){
+        console.log('ois')
+    }
+
+    obtemIMC(){
+
+    }
+
+    obtemMediaValida(){
+
+    }
+
+}
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
+console.log(atleta)
+
+/*
+calculaCategoria(), para calcular a categoria do atleta;
+calculaIMC(), para calcular o IMC do atleta;
+calculaMediaValida(), para calcular a média válida do atleta.
+obtemNomeAtleta(), que retorna o nome do atleta
+obtemIdadeAtleta(), que retorna a idade do atleta
+obtemPesoAtleta(), que retorna o peso do atleta
+obtemNotasAtleta(), que retorna as notas do atleta
+obtemCategoria(), que retorna a categoria do atleta
+obtemIMC(), que retorna o IMC do atleta
+obtemMediaValida(), que retorna a média válida do atleta
+*/
+
+/* 
+Nome: Cesar Abascal
+Idade: 30
+Peso: 80
+Altura: 1.7
+Notas: 10,9.34,8.42,10,7.88
+Categoria: Adulto
+IMC: 27.68166089965398
+Média válida: 8.766666666666666
+*/
